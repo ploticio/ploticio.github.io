22224e2a382bd9abd15ac400d4069bdf6885e1b8
Darren <darrentran001@gmail.com>
2022-06-15 07:24:13 -0700

    Rename animations

--- app.js
+++
@@ -1,8 +0,0 @@
-const swup = new Swup();
-
-const tl = gsap.timeline()
-
-tl.fromTo(".text", {duration: 1.5, opacity: 0, delay: 0.25}, {duration: 1, opacity: 1, stagger: .75})
-tl.to(".text", {duration: 1, opacity: 0, delay: .3, stagger: .5})
-tl.to(".intro", {duration: 0.5, opacity: 0})
-tl.to(".intro", {y: "-100%"})
